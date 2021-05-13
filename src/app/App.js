9d49f1e56f27f4b4f1aee603ed44d152161ff23e
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-05-13 15:39:02 +0200

    Working microscope

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,13 @@ import React, { useEffect, useState } from 'react';
 // Libs
 import gsap from 'gsap';
 
+import { TweenLite } from 'gsap/all';
 import { Draggable } from 'gsap/Draggable';
 
-import bcp from '../assets/img/bcp_490_x36/index';
+// Assets
+import bcp from '../assets/img/bcp_490_x36';
 
 import './App.css';
-import { TweenLite } from 'gsap/all';
 
 gsap.registerPlugin(Draggable);
 
@@ -32,7 +33,6 @@ const App = () => {
 
     setRotation(0);
   };
-
   return (
     <div className='container'>
       <p>{Math.round(rotation)}deg</p>
@@ -41,30 +41,40 @@ const App = () => {
       <div className='wheel'></div>
 
       <div className='lense'>
-        {bcp.map((image, i) => {
-          const sectionDeg = 360 / 35;
+        {bcp.map((image, i, arr) => {
+          /**
+           *
+           * On the images taken here, the set of images make a circle of 360deg, if the images only make 180deg of the circle
+           * you will need to map through the images another time to make the last 180deg.
+           *
+           * Changes to make on this circumstances
+           *   - The circle will be splitted in two, so the sectionDeg is not 360deg divided by the array length by 180deg.
+           *   - An offset of 180deg on the second section should be added otherwise it will overlap.
+           */
+
+          const sectionDeg = 360 / arr.length;
 
           const sectionPercent = (rotation / sectionDeg) % 1;
           const index = Math.floor(rotation / sectionDeg);
 
-          const visible = i === index;
+          const curr = i === index;
 
           const getVisibility = () => {
-            return visible ? 'visible' : 'hidden';
+            return curr ? 'visible' : 'hidden';
           };
 
           const getRotation = () => {
-            return visible ? `rotate(${sectionPercent * sectionDeg}deg)` : '';
+            return curr ? `rotate(${sectionPercent * sectionDeg}deg)` : '';
           };
 
           const style = {
             zIndex: 10 + i,
             visibility: getVisibility(),
             transform: getRotation(),
-            opacity: index + 1 === i ? 0 : 1,
+            opacity: 1,
           };
 
-          return <img className='img' style={{ ...style }} src={image} key={i} alt='' />;
+          return <img className={`img ${curr ? 'yes' : ''}`} style={{ ...style }} src={image} key={i} alt='' />;
         })}
       </div>
     </div>
